6ecef5496b83f24ab91696400d3c1a69726de899
Manole <manole.rotaru@finra.org>
2016-06-14 13:41:49 -0400

    Add more challenges

---
+++ src/Index.java
@@ -0,0 +1,23 @@
+public class Index {
+  /**
+   *  {@link Q21}
+   *    -  A list of n string, each of length n, is sorted into lexicographic order using the merge-sort algorithm.
+   *
+   *  {@link Q22}
+   *    -  Find a triplet a, b, c such that a^2 = b^2 + c^2. Variations of this problem like find a
+   *    triplet with sum equal to 0. Find a pair with given sum. All such questions are efficiently
+   *    solved using hashing.
+   *    - Rotate a matrix by 90 degree. (FIXME)
+
+   *
+   *  {@link Q23}
+   *    - Reverse a linked list in groups of size k.
+   *
+   *  {@link Q24}
+   *    - Implement a stack with push(), pop() and min() in O(1) time.
+   *
+   *  {@link Q25}
+   *    - Convert a BST into a DLL in place.
+   *
+   */
+}
